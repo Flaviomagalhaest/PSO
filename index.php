7dc28cbf8ff1a629e96e383965c14b198c76c437
Flaviomagalhaest <flaviomagalhaest@gmail.com>
2017-04-17 16:34:43 -0300

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-    <a href="gmaps.php"><input type="submit" class="button" value="PSO com GoogleMaps"></a>
-    <a href="classico.php"><input type="submit" class="button" value="PSO Clássica"></a>
-</body>
