0a6b96edd675fc3863d3ade952882c31bbeaf379
Flavio Magalhães <flavio.magalhaes@fcamara.com.br>
2018-03-18 16:46:53 -0300

    Continuação desenvolvimento de função execute da psoclassic

--- psoweb/py/classico/PSOClassic.py
+++ psoweb/py/classico/PSOClassic.py
@@ -1,17 +1,28 @@
 import IndivClass
 
+indivs = []
+gbest = ''
+
 config {
     'nrIteracoes' : 100,
 }
 
 def inicializa(pontos,objetivo):
-    indivs = []
-    gbest = ''
     #Transformando os pontos recebidos em individuos. Guardando numa lista
     #Calculando valor de fitness em seu construtor
     for ponto in pontos:
         indiv.append(IndivClass(ponto, objetivo))
 
+
 def execute(objetivo):
+    #Calculo de fitness e atualizacao do pbest
+    for indiv in indivs:
+        indiv.calculaFitness(objetivo)
+        indiv.checaPbest()
+    
+    #Calcula gbest
+
+def defineGbest():
+    
 
     
