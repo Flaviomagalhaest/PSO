5cdda12df0180b9dfc2c7fdd518d023d6d864a4e
Flavio <flaviomagalhaest@gmail.com>
2018-03-30 21:11:04 -0300

    Retirando lib 'py' do projeto para que seja criado um repositório único para ela, e usá-la como sub-tree do projeto principal

--- psoweb/py/ConfigConstants.py
+++
@@ -1,9 +0,0 @@
-from .Config import Config_API, Config_PSO
-
-def inicializaConfig(): 
-    return Config_API(
-        {'key' : 'AIzaSyC5wyAhlPFnEheBiT8i-XjpAajZ7i93eVQ',
-        'txtEnderecos' : 'C:\\wamp64\\www\\pso\\py\\enderecos.txt',
-        'transporte' : 'driving',
-        'limitDia' : 2500,
-        'limitRequi' : 100 })
